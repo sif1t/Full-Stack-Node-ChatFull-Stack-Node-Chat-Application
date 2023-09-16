af8fca48ddebb97d02ec8ee88edbcf53bd15f44e
arifeensifat01 <arifeen.sifat01@gmail.com>
2023-09-16 15:14:55 +0600

    chat application lesson-5 complite

---
+++ middlewares/common/checkLogin.js
@@ -0,0 +1,58 @@
+const jwt = require("jsonwebtoken");
+
+// auth guard to protect routes that need authentication
+const checkLogin = (req, res, next) => {
+  let cookies =
+    Object.keys(req.signedCookies).length > 0 ? req.signedCookies : null;
+
+  if (cookies) {
+    try {
+      token = cookies[process.env.COOKIE_NAME];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded;
+
+      // pass user info to response locals
+      if (res.locals.html) {
+        res.locals.loggedInUser = decoded;
+      }
+      next();
+    } catch (err) {
+      if (res.locals.html) {
+        res.redirect("/");
+      } else {
+        res.status(500).json({
+          errors: {
+            common: {
+              msg: "Authentication failure!",
+            },
+          },
+        });
+      }
+    }
+  } else {
+    if (res.locals.html) {
+      res.redirect("/");
+    } else {
+      res.status(401).json({
+        error: "Authetication failure!",
+      });
+    }
+  }
+};
+
+// redirect already logged in user to inbox pabe
+const redirectLoggedIn = function (req, res, next) {
+  let cookies =
+    Object.keys(req.signedCookies).length > 0 ? req.signedCookies : null;
+
+  if (!cookies) {
+    next();
+  } else {
+    res.redirect("/inbox");
+  }
+};
+
+module.exports = {
+  checkLogin,
+  redirectLoggedIn,
+};
